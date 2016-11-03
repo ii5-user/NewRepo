4bcfa84501ee174de16f16db8bcf200e0b76929b
Sujit Soni <sujit.soni@ii5.local>
2016-11-03 16:18:39 +0530

    initial commit

---
+++ src/NewTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+
+public class NewTest {
+	@Test
+	public void testApp1()
+	{
+		System.out.println("Testingert App");
+	}
+
+
+	@Test
+	public void testLogin1()
+	{
+		System.out.println("Login Test");
+	}
+
+}
---
+++ src/SampleTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void testApp1()
+	{
+		System.out.println("Testing App");
+	}
+
+
+	@Test
+	public void testLogin1()
+	{
+		System.out.println("Login Test");
+	}
+
+}
